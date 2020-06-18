ac050a54cfc78b4652e18f368ebc0741accd77b9
Nicholas Henry <nicholas.henry@gmail.com>
2020-06-18 10:40:19 -0400

    Step 3: Defining and Attaching The Telemetry Handler

---
+++ lib/quantum/telemetry/metrics.ex
@@ -0,0 +1,9 @@
+defmodule Quantum.Telemetry.Metrics do
+  require Logger
+  alias Quantum.Telemetry.StatsdReporter
+
+  def handle_event([:phoenix, :request], %{duration: dur}, metadata, _config) do
+    # do some stuff like log a message or report metrics to a service like StatsD
+    Logger.info("Received [:phoenix, :request] event. Request duration: #{dur}, Route: #{metadata.request_path}")
+  end
+end
