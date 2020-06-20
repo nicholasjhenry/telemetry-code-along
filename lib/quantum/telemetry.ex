31fb34cac03ba497542d97f5b7f3b5980ae3eb70
Nicholas Henry <nicholas.henry@gmail.com>
2020-06-20 12:33:24 -0400

    Add Phoenix event metrics

--- lib/quantum/telemetry.ex
+++ lib/quantum/telemetry.ex
@@ -16,7 +16,36 @@ defmodule Quantum.Telemetry do
 
   defp metrics do
     [
-      # coming soon!
+      summary(
+        "phoenix.router_dispatch.stop.duration",
+        unit: {:native, :millisecond},
+        tags: [:plug, :plug_opts]
+      ),
+
+      counter(
+        "phoenix.router_dispatch.stop.count",
+        tag_values: &__MODULE__.endpoint_metadata/1,
+        tags: [:plug, :plug_opts, :status]
+      ),
+
+      counter(
+        "phoenix.error_rendered.count",
+        tag_values: &__MODULE__.error_request_metadata/1,
+        tags: [:request_path, :status]
+      ),
+
+      counter(
+        "phoenix.socket_connected.count",
+        tags: [:endpoint]
+      )
     ]
+  end
+
+  def endpoint_metadata(%{conn: %{status: status}, plug: plug, plug_opts: plug_opts}) do
+    %{status: status, plug: plug, plug_opts: plug_opts}
+  end
+
+  def error_request_metadata(%{conn: %{request_path: request_path}, status: status}) do
+    %{status: status, request_path: request_path}
   end
 end
